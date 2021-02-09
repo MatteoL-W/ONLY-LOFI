43ce951facf9854c87c7da7f15f4722f879c5860
Matteo <matteo.ftt@gmail.com>
2021-02-09 16:45:04 +0100

    Ajout des fonts

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,11 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/fonts/icons.css">
+    <link rel="stylesheet" href="/fonts/bariol.css">
+    <link rel="stylesheet" href="/fonts/bahnschrift.css">
     <link rel="stylesheet" href="/css/style.css">
 
     <title>ONLY-LOFI</title>
 </head>
 <body>
+    <header>
+        <i class='demo-icon icon-music'></i>
+    </header>
 
 @yield('content')
 
