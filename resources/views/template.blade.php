9d26935a5be4fc19c2be502337a1237520fedef7
Styrau <matteo.styrau@gmail.com>
2021-03-08 18:24:40 +0100

    test commit

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -16,7 +16,7 @@
 </head>
 <body>
     <header>
-        <div class='header__left'><img src="/assets/inline-logo.svg" alt="Logo ONLYLOFI" class='header__left-logo'></div>
+        <div class='header__left'><a href="/"><img src="/assets/inline-logo.svg" alt="Logo ONLYLOFI" class='header__left-logo'></a></div>
         <div class='header__right'>
             <input type="search" name="" id="" class='header__right-search'>
             <div class="header__right-burger">
@@ -29,9 +29,9 @@
     <div id="menu" class='menu'>
         <div class="menu__grid">
             <div class="menu__grid-links">
-                <a href="#" class='links-main' data-tilt data-tilt-max="10">Main page <div><i class='icon-music'></i></div></a>
-                <a href="#" class='links-library' data-tilt data-tilt-max="10">Library <div><i class='icon-playlist'></i></div></a>
-                <a href="#" class='links-upload' data-tilt data-tilt-max="10">Upload <div><i class='icon-triangle'></i></div></a>
+                <a href="/" class='links-main' data-tilt data-tilt-max="10">Main page <div><i class='icon-music'></i></div></a>
+                <a href="song" class='links-library' data-tilt data-tilt-max="10">Library <div><i class='icon-playlist'></i></div></a>
+                <a href="upload" class='links-upload' data-tilt data-tilt-max="10">Upload <div><i class='icon-triangle'></i></div></a>
                 <a href="#" class='links-myacc' data-tilt data-tilt-max="10">My account <div><i class='icon-avatar'></i></div></a>
                 <a href="#" class='links-disconnect' data-tilt data-tilt-max="10">Disconnect <div><i class='icon-fleche'></i></div></a>
             </div>
