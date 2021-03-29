1840c9441372411dc05ba70189b6122c024740eb
Matteo <matteo.ftt@gmail.com>
2021-03-29 18:52:14 +0200

    be4 pull

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="/fonts/icons.css">
     <link rel="stylesheet" href="/fonts/bariol.css">
     <link rel="stylesheet" href="/fonts/bahnschrift.css">
